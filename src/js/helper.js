c1597ef8d55d3a93060881da4b5c64316c99c72e
wojtek <wswistak@student.agh.edu.pl>
2023-11-13 19:21:56 +0100

    add env

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,10 +1,11 @@
+import "dotenv/config";
+
 export const getJSON = async function (url) {
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmY1NGRkN2QxMDE1NmUyNWIyNDliMzAzZWQ0OTUyMyIsInN1YiI6IjY0NjUyZGVmZThkMDI4MDExYmU4ZjM3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q_3z-nY50EMg9fe2e218LYQn6GN-f5783CZKcVIJvlo",
+      Authorization: process.env.AUTH_KEY,
     },
   };
   try {
